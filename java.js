5d48cf9cba840fa3878a98bb570735204c78a4f4
jacob199524 <37050891+jacob199524@users.noreply.github.com>
2023-06-30 01:16:26 -0700

    Updated all 3 files

--- java.js
+++ java.js
@@ -1,25 +1,31 @@
 function getWeather() {
     const city = document.getElementById("city").value;
     const apiKey = "31e931352393e4bc553f77d1819d7c08"; // Replace with your OpenWeatherMap API key
-
+  
     // Make the API request using the Fetch API
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error("Network response was not ok");
-            }
-            return response.json();
-        })
-        .then(data => {
-            document.getElementById("result").innerHTML = "Current temperature in " + city + " is " + data.main.temp + " Celsius";
-        })
-        .catch(error => {
-            document.getElementById("result").innerHTML = "Error fetching weather data: " + error.message;
-        });
-
+      .then(response => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then(data => {
+        const resultElement = document.getElementById("result");
+        const temperatureCelsius = data.main.temp;
+        const temperatureFahrenheit = (temperatureCelsius * 9 / 5) + 32;
+        resultElement.innerHTML = "Current temperature in " + city + " is " + temperatureFahrenheit.toFixed(2) + " Fahrenheit";
+        resultElement.style.display = "block"; // Show the result element
+      })
+      .catch(error => {
+        const resultElement = document.getElementById("result");
+        resultElement.innerHTML = "Error fetching weather data: " + error.message;
+        resultElement.style.display = "block"; // Show the result element even if there's an error
+      });
+  
     // reset the form
     document.getElementById("city").value = "";
-
+  
     // prevent page from reloading
     return false;
-}
+  }
